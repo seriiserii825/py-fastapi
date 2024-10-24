bdc165743c43a96b51dc8bb296159ec02097de3e
serii burduja <seriiburduja@gmail.com>
2024-10-24 18:31:56 +0300

    feat: query params

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hotels")
+@app.get("/hotel/{hotel_id}")
 
-def get_hotels():
-    return {"hotels": "List of hotels"}
+def get_hotel(hotel_id: int, date_from, date_to):
+    return {"hotel_id": hotel_id, "date_from": date_from, "date_to": date_to}
